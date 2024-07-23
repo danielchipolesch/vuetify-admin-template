8256a96cf06ded403097e3ac5946212aed84f628
Daniel Chipolesch <danielchipolesch@gmail.com>
2024-07-22 21:59:52 -0300

    Criação de avatar no canto direito para interação do usuário bem como seu respectivo menu, e criação de ícones para interação também.

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,8 +1,14 @@
 <script setup>
 
-import { inject } from 'vue';
+import { ref, inject } from 'vue';
 
 const toggleDrawerSidebar = inject('toggleDrawerSidebar');
+
+const menuItems = ref([
+  { title: 'Dashboard', icon: 'mdi-home', to: '/'},
+  { title: 'Dashboard', icon: 'mdi-home', to: '/'},
+  { title: 'Sair', icon: 'mdi-login', to: '/'}
+])
 
 </script>
 
@@ -14,9 +20,31 @@ const toggleDrawerSidebar = inject('toggleDrawerSidebar');
 
         <v-spacer></v-spacer>
 
-        <template v-if="$vuetify.display.mdAndUp">
-          <v-btn icon="mdi-magnify" variant="text"></v-btn>
-          <v-btn icon="mdi mdi-account" variant="text"></v-btn>
+        <template v-if="$vuetify.display.mdAndUp" #append>
+          <v-btn icon="mdi-magnify" ripple="true"></v-btn>
+          <v-btn icon class="mr-4" ripple="true">
+            <v-badge dot color="error">
+              <v-icon icon="mdi-bell-outline"></v-icon>
+            </v-badge>
+          </v-btn>
+          <v-menu class="cursor-pointer">
+            <template #activator="{props}">
+              <v-avatar v-bind="props" class="cursor-pointer mr-5">
+                <v-img cover src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+              </v-avatar>
+            </template>
+            <v-list :lines="false" density="compact" nav>
+              <v-list-item
+                v-for="(item, index) in menuItems"
+                :key="index"
+                :value="index"
+                :prepend-icon="item.icon"
+                ripple="true"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </template>
 
     </v-app-bar>
